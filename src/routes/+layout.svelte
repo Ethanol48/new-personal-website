894682049dd38393cb3d512da86cfb97cb950571
Ethan Rouimi <contact@ethan-rouimi.com>
2024-11-01 16:15:16 +0100

    no me acuerdo si esto es necesario

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	import '../app.css';
 	import '@fortawesome/fontawesome-free/css/all.min.css';
+
 	let { children } = $props();
+
+
 </script>
 
-<main class="dark mt-2 flex h-screen w-screen max-w-[450px] flex-col items-center gap-2">
+<main class="dark mt-2 flex h-screen w-screen max-w-[450px] flex-col items-center gap-2 relative">
 	{@render children()}
 </main>
+
+
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,14 @@
 	import Title from '$lib/Title.svelte';
 	import Experience from '$lib/Experience.svelte';
 	import Studies from '$lib/Studies.svelte';
+
 </script>
 
-<div class="flex h-full w-full flex-col items-center gap-8 p-5">
+<div class="flex h-full w-full flex-col items-center gap-8 p-5 relative">
 	<Title />
 	<Experience />
 	<Studies />
 </div>
+
+
+
