b18f63f78fa6ab9b5e04c359b14ebe5c28615c7e
Ethan Rouimi <contact@ethan-rouimi.com>
2024-11-06 11:58:55 +0100

    new layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import '../app.css';
 	import '@fortawesome/fontawesome-free/css/all.min.css';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import svelte from '$lib/assets/prog-icons/svelte-icon.svg';
+	import shadcn from '$lib/assets/shadcn.png';
 
 	let { children } = $props();
 </script>
@@ -14,6 +16,15 @@
 	}}
 />
 
-<main class="dark relative flex h-screen w-screen max-w-[450px] flex-col items-center gap-2 pt-2">
+<main class="dark relative flex h-screen w-screen flex-col items-center gap-2 pt-2">
 	{@render children()}
+
+	<div class="flex w-full items-center justify-center gap-2 py-10">
+		<p>Made with</p>
+		<i class="fa-solid fa-heart text-[#bf2222]"></i>
+		<p style="margin: 0px;">using</p>
+		<img src={svelte} class="size-6" alt="svelte" />
+		<p style="margin: 0px;">and</p>
+		<img src={shadcn} class="size-6 rounded" alt="svelte-shadcn" />
+	</div>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,14 @@
 	let duration = 700;
 </script>
 
-<div class="relative flex h-full w-full flex-col items-center gap-8 p-5">
-	<Title {duration} {delay} />
-	<AboutMe {duration} delay={delay + 500} />
-	<Experience {duration} delay={delay + 1000} />
+<div
+	class="relative flex h-full w-full max-w-[450px] flex-col items-center gap-8 p-10 lg:max-w-[900px] lg:flex-row lg:items-start"
+>
+	<div class="flex w-full flex-col gap-8 lg:w-1/2">
+		<Title {duration} {delay} />
+		<AboutMe {duration} delay={delay + 500} />
+	</div>
+	<div class="w-full lg:w-1/2">
+		<Experience {duration} delay={delay + 1000} />
+	</div>
 </div>
